1a3f137c419777e670a0b138bad67f8a319e9e0d
zanzen <olsencortuna@gmail.com>
2024-10-14 09:10:23 +0800

    fix log out

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react";
 import { auth, db } from "@/firebase"; // Import your firebase config
 import { doc, getDoc } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
-import Account from "./Account";
+import { useRouter } from "next/navigation";
 import GetImage from "../GetImage";
 import MessageIndicator from "./MessageIndicator";
 
-const Header = () => {
-  // const handleSignOut = async () => {
-  //   await auth.signOut();
-  // };
+const Header = () => { 
+  const router = useRouter();
+  const handleSignOut = async () => {
+    await auth.signOut();
+    router.push("/sign-in");
+  };
   const [userData, setUserData] = useState<any>(null);
   const [showNotif, setShowNotif] = useState<boolean>(false);
 
@@ -42,12 +44,12 @@ const Header = () => {
     <span className="w-full h-14 bg-gray-100 justify-between px-5 items-center border-b border-gray-300 hidden md:flex">
       <div className="flex items-center gap-4 ml-auto">
         <MessageIndicator />
-        {/* <button
-          className="flex gap-2 w-full border-b-2 p-3 font-semibold text-red-700 hover:bg-primary hover:text-white"
+        <button
+          className="btn btn-xs btn-error btn-outline rounded-none"
           onClick={handleSignOut}
         >
           <h1>Sign Out</h1>
-        </button> */}
+        </button>
         <details className="dropdown dropdown-end">
           <summary
             tabIndex={0}
@@ -58,7 +60,7 @@ const Header = () => {
               <GetImage storageLink="settings/brgyLogo" />
             </div>
           </summary>
-          <Account userData={userData} />
+          {/* <Account userData={userData} /> */}
         </details>
       </div>
     </span>
